dc50d55705101fef01829d7fbd0ed40446f24e8b
atul17-11 <84310838+atul17-11@users.noreply.github.com>
2022-10-14 09:08:42 +0000

    Update changelog.sql

--- changelog.sql
+++ changelog.sql
@@ -2,28 +2,29 @@
 
 --changeset your.name:1 labels:example-label context:example-context
 --comment: example comment
-create table person (
-    id int identity(1,1) PRIMARY KEY,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
+--create table person (
+ --   id int identity(1,1) PRIMARY KEY,
+  --  name varchar(50) not null,
+  --  address1 varchar(50),
+  --  address2 varchar(50),
+   -- city varchar(30)
+--)
 --rollback DROP TABLE person;
 
 --changeset your.name:2 labels:example-label context:example-context
 --comment: example comment
-create table company (
-    id int identity(1,1) PRIMARY KEY,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
+--create table company (
+ --   id int identity(1,1) PRIMARY KEY,
+ --   name varchar(50) not null,
+   -- address1 varchar(50),
+    --address2 varchar(50),
+    --city varchar(30)
+--)
 --rollback DROP TABLE company;
 
 --changeset other.dev:3 labels:example-label context:example-context
 --comment: example comment
-alter table person add column country varchar(2)
+--alter table person add column country varchar(2)
 --rollback ALTER TABLE person DROP COLUMN country;
+alter table person add column state varchar(2)
 
