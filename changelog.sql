78e5d90b49038aeada79b11403fca9ae3902c99c
atul17-11 <84310838+atul17-11@users.noreply.github.com>
2022-10-20 12:03:43 +0000

    Update changelog.sql

--- changelog.sql
+++ changelog.sql
@@ -1,30 +1,11 @@
---liquibase formatted sql
-
---changeset your.name:1 labels:example-label context:example-context
---comment: example comment
-create table person (
-    ID INT NOT NULL IDENTITY(1, 1),
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
---rollback DROP TABLE person;
-
---changeset your.name:2 labels:example-label context:example-context
---comment: example comment
-create table company (
-    ID INT NOT NULL IDENTITY(1, 1),
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
-)
---rollback DROP TABLE company;
-
---changeset other.dev:3 labels:example-label context:example-context
---comment: example comment
---alter table person add column country varchar(2)
---rollback ALTER TABLE person DROP COLUMN country;
---alter table person add column state varchar(2)
-
+<?xml version="1.0" encoding="UTF-8"?>  
+<databaseChangeLog
+	xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:ext="http://www.liquibase.org/xml/ns/dbchangelog-ext"
+	xmlns:pro="http://www.liquibase.org/xml/ns/pro"
+	xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+		http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-latest.xsd
+		http://www.liquibase.org/xml/ns/dbchangelog-ext http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-ext.xsd
+		http://www.liquibase.org/xml/ns/pro http://www.liquibase.org/xml/ns/pro/liquibase-pro-latest.xsd">
+</databaseChangeLog>
